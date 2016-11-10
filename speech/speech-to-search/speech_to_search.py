32dcf436598fedb39d86d7117dc6df386994a6b3
Gus Class <class@google.com>
2016-11-10 13:39:47 -0800

    Adds fixes for bugs found in grpc

--- speech/speech-to-search/speech_to_search.py
+++ speech/speech-to-search/speech_to_search.py
@@ -19,6 +19,7 @@ from __future__ import division
 import contextlib
 import re
 import signal
+import sys
 import subprocess
 import threading
 
@@ -33,8 +34,8 @@ import pyaudio
 from six.moves import queue
 
 
-# Configuration for Custom Search API calls.
-API_KEY = 'YOUR_API_KEY'
+# Configuration for Custom Search Engine (CSE) API calls.
+CSE_KEY = 'YOUR_API_KEY' # Replace with your developer key
 CSE_ID = '00000000012345:aaa7bbb_cc' # Replace with your CSE ID
 USE_OSX_SAY = True # Issues on OSX w/ pyttsx; if true, uses 'Say' alternate
 
@@ -48,6 +49,7 @@ CHUNK = int(RATE / 10)  # 100ms
 # * https://g.co/cloud/speech/limits#content
 DEADLINE_SECS = 60 * 3 + 5
 SPEECH_SCOPE = 'https://www.googleapis.com/auth/cloud-platform'
+CSE_SCOPE = 'https://www.googleapis.com/auth/customsearch'
 
 # Global for keeping around the Google search results.
 _results = {}
@@ -85,12 +87,10 @@ def _audio_data_generator(buff):
         A chunk of data that is the aggregate of all chunks of data in `buff`.
         The function will block until at least one data chunk is available.
     """
-    while True:
+    stop = False
+    while not stop:
         # Use a blocking get() to ensure there's at least one chunk of data
         chunk = buff.get()
-        if not chunk:
-            # A falsey value indicates the stream is closed.
-            break
         data = [chunk]
 
         # Now consume whatever other data's still buffered.
@@ -99,22 +99,31 @@ def _audio_data_generator(buff):
                 data.append(buff.get(block=False))
             except queue.Empty:
                 break
+
+        # If `_fill_buffer` adds `None` to the buffer, the audio stream is
+        # closed. Yield the final bit of the buffer and exit the loop.
+        if None in data:
+            stop = True
+            data.remove(None)
         yield b''.join(data)
 
 
-def _fill_buffer(audio_stream, buff, chunk):
+def _fill_buffer(audio_stream, buff, chunk, stoprequest):
     """Continuously collect data from the audio stream, into the buffer."""
     try:
-        while True:
+        while not stoprequest.is_set():
             buff.put(audio_stream.read(chunk))
     except IOError:
-        # This happens when the stream is closed. Signal that we're done.
+        pass
+    finally:
+        # Add `None` to the buff, indicating that a stop request is made.
+        # This will signal `_audio_data_generator` to exit.
         buff.put(None)
 
 
 # [START audio_stream]
 @contextlib.contextmanager
-def record_audio(rate, chunk):
+def record_audio(rate, chunk, stoprequest):
     """Opens a recording stream in a context manager."""
     audio_interface = pyaudio.PyAudio()
     audio_stream = audio_interface.open(
@@ -132,25 +141,26 @@ def record_audio(rate, chunk):
     # This is necessary so that the input device's buffer doesn't overflow
     # while the calling thread makes network requests, etc.
     fill_buffer_thread = threading.Thread(
-        target=_fill_buffer, args=(audio_stream, buff, chunk))
+        target=_fill_buffer, args=(audio_stream, buff, chunk, stoprequest))
     fill_buffer_thread.start()
 
     yield _audio_data_generator(buff)
 
-    audio_stream.stop_stream()
-    audio_stream.close()
     fill_buffer_thread.join()
+    audio_stream.close()
     audio_interface.terminate()
 # [END audio_stream]
 
 
-def request_stream(data_stream, rate):
+def request_stream(data_stream, rate, interim_results=True):
     """Yields `StreamingRecognizeRequest`s constructed from a recording audio
     stream.
 
     Args:
         data_stream: A generator that yields raw audio data to send.
         rate: The sampling rate in hertz.
+        interim_results: Whether to return intermediate results, before the
+            transcription is finalized.
     """
     # The initial request must contain metadata about the stream, so the
     # server knows how to interpret it.
@@ -159,12 +169,12 @@ def request_stream(data_stream, rate):
         # https://goo.gl/KPZn97 for the full list.
         encoding='LINEAR16',  # raw 16-bit signed LE samples
         sample_rate=rate,  # the rate in hertz
-        # See
-        # https://g.co/cloud/speech/docs/best-practices#language_support
+        # See http://g.co/cloud/speech/docs/languages
         # for a list of supported languages.
         language_code='en-US',  # a BCP-47 language tag
     )
     streaming_config = cloud_speech.StreamingRecognitionConfig(
+        interim_results=interim_results,
         config=recognition_config,
     )
 
@@ -176,13 +186,16 @@ def request_stream(data_stream, rate):
         yield cloud_speech.StreamingRecognizeRequest(audio_content=data)
 
 
-def listen_print_loop(recognize_stream):
+def listen_print_loop(recognize_stream, stoprequest):
     global _index
     global _results
+    last_search = ''
     for resp in recognize_stream:
         if resp.error.code != code_pb2.OK:
             raise RuntimeError('Server error: ' + resp.error.message)
 
+        if not resp.results:
+            continue
         update_search = True
         say_update = False
 
@@ -193,6 +206,7 @@ def listen_print_loop(recognize_stream):
                for alt in result.alternatives):
             update_search = False
             print('Exiting..')
+            stoprequest.set()
             break
 
         # Say "Next" to reach the next result.
@@ -206,10 +220,12 @@ def listen_print_loop(recognize_stream):
 
         # Retrieve search results using speech result
         if update_search and resp.results:
-            for result in resp.results:
-                print('Searching for ' + result.alternatives[0].transcript)
-                google_search(result.alternatives[0].transcript, num=10)
-                if len(result.alternatives) > 0:
+            first_result = resp.results[0].alternatives[0]
+            if first_result is not last_search:
+                print('Searching for ' + first_result.transcript)
+                lastSearch = first_result.transcript
+                google_search(lastSearch, num=10)
+                if len(first_result.transcript) > 0:
                     say_update = True
                     _index = 0
 
@@ -231,16 +247,24 @@ def say_result(index):
 def google_search(search_term, **kwargs):
     """Uses the custom search to query for the user's utterance."""
     global _results
-    service = build("customsearch", "v1", developerKey=API_KEY)
+    service = build("customsearch", "v1", developerKey=CSE_KEY)
     res = service.cse().list(q=search_term, cx=CSE_ID, **kwargs).execute()
     _results = res['items']
 
 def main():
     with cloud_speech.beta_create_Speech_stub(
             make_channel('speech.googleapis.com', 443)) as service:
+
+        # stoprequest is event object which is set in `listen_print_loop`
+        # to indicate that the trancsription should be stopped.
+        #
+        # The `_fill_buffer` thread checks this object, and closes
+        # the `audio_stream` once it's set.
+        stoprequest = threading.Event()
+
         # For streaming audio from the microphone, there are three threads.
         # First, a thread that collects audio data as it comes in
-        with record_audio(RATE, CHUNK) as buffered_audio_data:
+        with record_audio(RATE, CHUNK, stoprequest) as buffered_audio_data:
             # Second, a thread that sends requests with that data
             requests = request_stream(buffered_audio_data, RATE)
             # Third, a thread that listens for transcription responses
@@ -252,8 +276,7 @@ def main():
 
             # Now, put the transcription responses to use.
             try:
-                listen_print_loop(recognize_stream)
-
+                listen_print_loop(recognize_stream, stoprequest)
                 recognize_stream.cancel()
             except face.CancellationError:
                 # This happens because of the interrupt handler
