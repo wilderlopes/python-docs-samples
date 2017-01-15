c09dcfdb00ece61b34be4df7efc02beaecf65676
Wilder Lopes <wilderlopes@gmail.com>
2017-01-15 22:31:43 +0100

    Understading the 'users' sample

--- appengine/standard/users/main.py
+++ appengine/standard/users/main.py
@@ -30,8 +30,12 @@ class MainPage(webapp2.RequestHandler):
         if user:
             nickname = user.nickname()
             logout_url = users.create_logout_url('/')
-            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
-                nickname, logout_url)
+            if nickname == 'wil@example.com':
+                greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+                    nickname, logout_url)
+            else:
+                greeting = 'No, {}! (<a href="{}">sign out</a>)'.format(
+                    nickname, logout_url)
         else:
             login_url = users.create_login_url('/')
             greeting = '<a href="{}">Sign in</a>'.format(login_url)
